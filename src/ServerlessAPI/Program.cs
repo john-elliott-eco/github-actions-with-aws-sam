ca0405a4bfb6c749de66f93124368895eef00cf0
John Elliott <john.elliott@ecoonline.com>
2025-04-15 12:19:03 +0100

    added endpoint

--- src/ServerlessAPI/Program.cs
+++ src/ServerlessAPI/Program.cs
@@ -2,6 +2,7 @@ using System.Text.Json;
 using Amazon;
 using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.DataModel;
+using ServerlessAPI.Entities;
 using ServerlessAPI.Repositories;
 
 
@@ -38,5 +39,30 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.MapGet("/", () => "Welcome to running ASP.NET Core Minimal API on AWS Lambda - Test");
+
+app.MapGet("/test", () =>
+{
+    var books = new List<Book> {
+            new Book
+            {
+                Id = Guid.NewGuid(),
+                Title = "Book 1",
+                ISBN = "1234567890",
+                Authors = new List<string> { "Author 1", "Author 2" },
+                CoverPage = "https://example.com/cover.jpg"
+            },
+            new Book
+            {
+                Id = Guid.NewGuid(),
+                Title = "Book 2",
+                ISBN = "0987654321",
+                Authors = new List<string> { "Author 3", "Author 4" },
+                CoverPage = "https://example.com/cover2.jpg"
+            }
+        };
+
+    return books;
+});
+
 
 app.Run();
