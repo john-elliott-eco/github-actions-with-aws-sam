e8dbe16348e00d30aa33ea0fb28c3d3330c1414d
John Elliott <john.elliott@ecoonline.com>
2025-04-15 15:09:21 +0100

    fixed timeout issue

---
+++ src/ServerlessAPI/Services/GetParams.cs
@@ -0,0 +1,27 @@
+﻿using Amazon.SimpleSystemsManagement;
+using Amazon.SimpleSystemsManagement.Model;
+
+namespace ServerlessAPI.Services
+{
+    public class GetParams
+    {
+        public async Task<Dictionary<string, string>> GetParametersAsync(string parameterName, bool decrypt)
+        {
+            using var client = new AmazonSimpleSystemsManagementClient();
+            var request = new GetParametersRequest
+            {
+                Names = new List<string> { parameterName },
+                WithDecryption = decrypt // Set to true if the parameter is encrypted
+            };
+
+            var response = await client.GetParametersAsync(request);
+
+            if (response.InvalidParameters.Count > 0)
+            {
+                throw new Exception($"Invalid parameters: {string.Join(", ", response.InvalidParameters)}");
+            }
+
+            return response.Parameters.ToDictionary(p => p.Name, p => p.Value);
+        }
+    }
+}
---
+++ src/ServerlessAPI/Services/SqsProcessor.cs
@@ -0,0 +1,58 @@
+﻿using Amazon.Lambda.Core;
+using Amazon.Lambda.SQSEvents;
+using static Amazon.Lambda.SQSEvents.SQSBatchResponse;
+
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace ServerlessAPI.Services;
+
+public class SqsProcessor
+{
+    public async Task<SQSBatchResponse> FunctionHandler(SQSEvent evnt, ILambdaContext context)
+    {
+        List<BatchItemFailure> _batchItemFailures = new();
+
+        if (evnt.Records.Count == 0)
+        {
+            context.Logger.LogLine("Empty SQS Event received");
+            return new SQSBatchResponse();
+        }
+
+        foreach (var message in evnt.Records)
+        {
+            BatchItemFailure? result = await ProcessMessageAsync(message, context);
+            if (result != null)
+            {
+                _batchItemFailures.Add(result);
+            }
+        }
+
+        return new SQSBatchResponse(_batchItemFailures);
+    }
+
+    private async Task<BatchItemFailure?> ProcessMessageAsync(SQSEvent.SQSMessage message, ILambdaContext context)
+    {
+        try
+        {
+            context.Logger.LogInformation($"Processed message {message.Body}");
+            // TODO: Do interesting work based on the new message
+
+            //get parameters from GetParams
+            var getParams = new GetParams();
+            var parameters = await getParams.GetParametersAsync("queue-name", false);
+            context.Logger.LogInformation($"Parameters: {string.Join(", ", parameters.Select(p => $"{p.Key}={p.Value}"))}");
+
+
+            await Task.CompletedTask; // Placeholder for actual async work
+            return null;
+
+        }
+        catch (Exception e)
+        {
+            context.Logger.LogError($"An error occurred - {e.Message}");
+            return new BatchItemFailure { ItemIdentifier = message.MessageId };
+        }
+    }
+}
